c70b0f182494bf6860219caf3c9731f3ed3afd48
Amanda Torres <amandatorr06@gmail.com>
2023-08-14 10:19:33 -0400

    Initial Commit

---
+++ github/githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubApp.swift
+//  github
+//
+//  Created by amanda torres💜. on 8/14/23.
+//
+
+import SwiftUI
+
+@main
+struct githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
